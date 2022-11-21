065bf2655f3246b652b75e8c624827f419945db0
Andrii Petlovanyi <33178699+andrii-petlovanyi@users.noreply.github.com>
2022-11-21 19:08:29 +0200

    added PropTypes for Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { Component } from 'react';
 import {
   SearchbarWrapper,
@@ -49,3 +50,7 @@ export class Searchbar extends Component {
     );
   }
 }
+
+Searchbar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
